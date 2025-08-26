b467f00c86d802a08dbd515257abdc963e9fcfda
tolikgorunovich234-byte <tolik.gorunovich234@gmail.com>
2025-08-26 22:24:36 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div style={{ padding: 24, background: "#111", color: "#0f0", fontSize: 24 }}>
-      ✅ Рендер работает. Если ты это видишь — JS ок.
+      ✅ Рендер работает! Если ты это видишь — всё ок.
     </div>
   );
 }

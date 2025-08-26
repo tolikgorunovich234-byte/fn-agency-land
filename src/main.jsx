a5109b0e6da52913a5a068a4da5f84f0ec280ba6
tolikgorunovich234-byte <tolik.gorunovich234@gmail.com>
2025-08-26 22:05:31 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import WTFunnelLanding from "./App.jsx";
+import WTFunnelLanding from "./App.jsx";  // именно .jsx
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(

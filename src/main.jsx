c9e856e0268d5109447c8309780cc37a9f5142dc
tolikgorunovich234-byte <tolik.gorunovich234@gmail.com>
2025-08-26 22:45:05 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+// import "./index.css"; // ⬅️ временно отключим, чтобы исключить влияние стилей
 
-console.log("BOOT: main.jsx loaded"); // маркер в консоли браузера
+console.log("BOOT: main.jsx loaded");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  // <React.StrictMode>   // ⬅️ тоже временно уберём, чтобы исключить двойной монтёж
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );

b22aaeeaf74f31ad420760f6f05533bd9ee81784
tolikgorunovich234-byte <tolik.gorunovich234@gmail.com>
2025-08-26 22:05:02 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: {
-    extend: {},
-  },
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: { extend: {} },
   plugins: [],
-}
+};

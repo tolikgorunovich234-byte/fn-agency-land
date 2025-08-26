5d4c8d2a2e455b52b960972603906f7d28433808
tolikgorunovich234-byte <tolik.gorunovich234@gmail.com>
2025-08-26 22:23:57 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import WTFunnelLanding from "./App.jsx";
-import "./index.css";
+import App from "./App.jsx";   // Импортируем App
+import "./index.css";          // Подключаем стили
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <WTFunnelLanding />
+    <App />   {/* Рендерим App */}
   </React.StrictMode>
 );
